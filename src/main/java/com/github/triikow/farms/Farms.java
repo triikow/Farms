bfa0c1a7d966a970d2431392101c2e8e307177a7
triikow <tristan.reynders@outlook.com>
2026-01-03 22:32:09 +0100

    Initial commit

---
+++ src/main/java/com/github/triikow/farms/Farms.java
@@ -0,0 +1,16 @@
+package com.github.triikow.farms;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Farms extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
